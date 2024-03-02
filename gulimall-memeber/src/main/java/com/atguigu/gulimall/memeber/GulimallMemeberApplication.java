5e06a377294b46d7b35b1f6349cdeb5789ce4bd3
xiayelailele2 <125011936+xiayelailele2@users.noreply.github.com>
2024-03-02 13:43:31 +0800

    项目初始结构创建

---
+++ gulimall-memeber/src/main/java/com/atguigu/gulimall/memeber/GulimallMemeberApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.memeber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallMemeberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallMemeberApplication.class, args);
+	}
+
+}
